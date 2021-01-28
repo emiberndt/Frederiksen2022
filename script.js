df0567c14c251579bc1c1662b17b9c6b5fbf053f
emiberndt <emi.berndt@gmail.com>
2021-01-28 16:02:55 +0000

    Background music

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// Background music
+let bgMusic = document.getElementById("bgMusic");
+let icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    if(bgMusic.paused){
+        bgMusic.play();
+        icon.src = "media/music/pause.png";
+    }else{
+        bgMusic.pause();
+        icon.src = "media/music/play.png";
+    }
+}
+
+// Sidebar navigation menu 
 document.querySelector(".burger-menu").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");
 });
+
